d600a85150605c96d257c4e88fe0fcb80472656a
Pavel <alyabev1998@gmail.com>
2022-02-09 23:15:35 +0300

    usual commit

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -5,9 +5,34 @@ import React from 'react';
 const MainContent = () => (
     <React.Fragment>
       <main className='main'>
-        Main
+        <AppFunc />
       </main>
       </React.Fragment>
   );
   
+  const AppFunc = () => {
+    const firstName = "Bob"
+    const lastName = "Ziroll"
+
+    const date = new Date()
+    const hours = date.getHours()
+    var timeOfDay
+
+    if (hours < 12) {
+      timeOfDay = "morning"
+      } else if (hours >= 12 && hours < 17) {
+      timeOfDay = "afternoon"
+      } else {
+      timeOfDay = "night"
+      }
+
+    return (
+    <div>
+    <h1>Hello {`${firstName} ${lastName}`}!</h1>
+
+    <h1>It is currently about {date.getHours() % 12} o'clock!  Good {timeOfDay}!</h1>
+    </div>
+    )
+    }
+
   export default MainContent;
