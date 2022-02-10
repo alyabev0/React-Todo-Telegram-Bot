8cf33b5f48661add630a00d5ab5786c2024a1d7f
Pavel <alyabev1998@gmail.com>
2022-02-10 17:59:12 +0300

    props

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,53 @@
 // import logo from './logo.svg';
 import './App.css';
 import React from 'react';
-import MainContent from './MainContent';
-import Footer from './Footer'
-import Header from './Header'
+import '../index.css';
+import ContactCard from "./ContactCard"
+// import MainContent from './MainContent';
+// import Footer from './Footer'
+// import Header from './Header'
 // import style from './style';
 // import ReactDOM from 'react-dom';
 
+function App() {
+  return (
+  <div className="contacts">
+    <ContactCard
+      name="Mr. Whiskerson"
+      imgUrl="http://placekitten.com/300/200"
+      phone="(212) 555-1234"
+      email="mr.whiskaz@catnap.meow"
+      />
+      <ContactCard
+      name="Fluffykins"
+      imgUrl="http://placekitten.com/400/200"
+      phone="(212) 555-2345"
+      email="fluff@me.com"
+      />
+      <ContactCard
+      name="Destroyer"
+      imgUrl="http://placekitten.com/400/300"
+      phone="(212) 555-3456"
+      email="ofworlds@yahoo.com"
+      />
+      <ContactCard
+      name="Felix"
+      imgUrl="http://placekitten.com/200/100"
+      phone="(212) 555-4567"
+      email="thecat@hotmail.com"
+      />
+  </div>
+  )
+  }
 
 
 
-const App = () => (
-  <div>
-    <Header />
-    <MainContent />
-    <Footer />
-    </div>
-);
+// const App = () => (
+//   <div>
+//     <Header />
+//     <MainContent />
+//     <Footer />
+//     </div>
+// );
 
 export default App;
-
---
+++ src/components/ContactCard.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+
+function ContactCard(props) {
+    console.log(props)
+return (
+<div className="contact-card">
+<img src={props.imgUrl}/>
+<h3>{props.name}</h3>
+<p>{props.phone}</p>
+<p>{props.email}</p>
+</div>
+)
+}
+
+export default ContactCard
